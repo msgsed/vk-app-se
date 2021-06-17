682434e3bf22a4943a904713330f61ce255f05f5
Sergey Dmitiev <msgsed@gmail.com>
2021-06-17 07:52:35 +0300

    first commit

---
+++ vk-app-se/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk-app-se
+//
+//  Created by Sergey Dmitriev on 17.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
